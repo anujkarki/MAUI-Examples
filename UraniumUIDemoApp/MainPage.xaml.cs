9c09e316b20e6d550b2e2ac75db9e494adbdf314
Anuj Karki <anujkarki553@gmail.com>
2024-09-27 16:37:45 +0545

    Time and Countdown Timer Implemented

--- UraniumUIDemoApp/MainPage.xaml.cs
+++ UraniumUIDemoApp/MainPage.xaml.cs
@@ -1,24 +1,63 @@
-﻿namespace UraniumUIDemoApp
+﻿using System.Timers;
+
+namespace UraniumUIDemoApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+        TimeSpan _timeRemaining;
+        private System.Timers.Timer _timer;
         public MainPage()
         {
             InitializeComponent();
+            setCurrentDateTime();
+            _timer = new System.Timers.Timer(1000);
+            _timer.Elapsed += TimerElapsed;
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
+            setDateTimeInLabel();
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
+        private void btnGetCurrentDate_Clicked(object sender, EventArgs e)
+        {
+            setCurrentDateTime(); 
+            setDateTimeInLabel();
+        }
+        void setCurrentDateTime()
+        {
+            var currentDate = DateTime.Now;
+            txtDate.Date = currentDate;
+            txtTime.Time = new TimeSpan(currentDate.Hour, currentDate.Minute, currentDate.Second);
+        }
+        DateTime setTime;
+        void setDateTimeInLabel()
+        {
+            string currentDateString = string.Empty;
+            string curDate = txtDate.Date?.ToString("MM/dd/yyyy") ?? string.Empty;
+            string curTime = txtTime.Time?.ToString(@"hh\:mm\:ss") ?? string.Empty;
+            currentDateString = curDate + ' ' + curTime;
+            //lblCounter.Text = currentDateString;
+            DateTime.TryParse(currentDateString, out setTime);
+            if(!_timer.Enabled)
+                _timer.Start();
+        }
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            _timeRemaining = setTime - DateTime.Now;
+            if (_timeRemaining.TotalSeconds > 0)
+            {
+                UpdateCountdownLabel();
+            }
             else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            {
+                _timer.Stop();
+                MainThread.BeginInvokeOnMainThread(() => lblCounter.Text = "Time's up!");
+            }
+        }
+        private void UpdateCountdownLabel()
+        {
+            MainThread.BeginInvokeOnMainThread(() => lblCounter.Text = _timeRemaining.ToString(@"hh\:mm\:ss"));
         }
     }
 
--- UraniumUIDemoApp/MauiProgram.cs
+++ UraniumUIDemoApp/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using UraniumUI;
 
 namespace UraniumUIDemoApp
 {
@@ -9,6 +10,8 @@ namespace UraniumUIDemoApp
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseUraniumUI()
+                .UseUraniumUIMaterial()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
